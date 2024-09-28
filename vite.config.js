58450863d8b4d53d355df34f991db2d90132ac4f
sebastiandion <swd7104@gmail.com>
2024-09-28 10:16:13 -0400

    Removed Animation

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio-project/', // Add this line for GitHub Pages
   plugins: [react()],
-  css : {
+  css: {
     modules: {
       localsConvention: "camelCase"
     }
   }
 })
+
